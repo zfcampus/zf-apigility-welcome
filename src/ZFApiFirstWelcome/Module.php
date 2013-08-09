4cca9fb4093a41647418ac7bde660309e91fd6ab
Ralph Schindler <ralph@ralphschindler.com>
2013-08-09 11:40:27 -0500

    Initial commit

---
+++ src/ZFApiFirstWelcome/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ZFApiFirst;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+}
